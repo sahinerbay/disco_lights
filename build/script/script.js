dbaf21a2b87b376ff5adc78e27384268a63f495e
sahinerbay <sahin@sahinerbay.com>
2017-10-26 21:28:19 +0300

    Create settings object and place slider function inside of create  namespace

--- build/script/script.js
+++ build/script/script.js
@@ -68,167 +68,181 @@ var dom = function () {
     };
 }();
 
-var container = function () {
-    var container = dom.createElement('div');
-    dom.setClass(container, 'container');
-    dom.append(document.body, container);
-    return container;
+var create = function () {
+
+    var container = function container() {
+        var container = dom.createElement('div');
+        dom.setClass(container, 'container');
+        dom.append(document.body, container);
+    };
+
+    var cells = function cells(numberOfBoxes) {
+        var container = dom.getElement('container', 0);
+        for (var i = 0; i < numberOfBoxes; i++) {
+            var box = dom.createElement('div');
+            dom.setClass(box, 'box');
+            dom.append(container, box);
+        }
+    };
+
+    var settingsBar = function settingsBar() {
+        // Create Setting Div //
+        var settings = dom.createElement('div');
+        dom.setClass(settings, 'settings');
+        dom.append(document.body, settings);
+
+        // Create Setting Container Div //
+        var settingsContainer = dom.createElement('div');
+        dom.setClass(settingsContainer, 'settings__container');
+        dom.append(settings, settingsContainer);
+    };
+
+    var slider = function slider(obj) {
+
+        // create slider-container-div //
+        var slider = dom.createElement('div'),
+            settings = dom.getElement('settings__container', 0),
+            container = dom.getElement('container', 0);
+
+        dom.setClass(slider, 'settings__container__' + obj.classname);
+        dom.append(settings, slider);
+
+        // create slider-title //
+        var sliderTitle = dom.createElement('h2');
+        sliderTitle.textContent = obj.title;
+        dom.setClass(sliderTitle, 'settings__container__' + obj.classname + '__title');
+        dom.append(slider, sliderTitle);
+
+        // create slider-input //
+        var sliderInput = dom.createElement('input');
+        dom.setAttributes(sliderInput, obj.attrs);
+        dom.append(slider, sliderInput);
+
+        // show current value //
+        var showValue = dom.createElement('p');
+        showValue.textContent = 'value: ' + sliderInput.value;
+        dom.setClass(showValue, 'settings__container__' + obj.classname + '__value');
+        dom.append(slider, showValue);
+
+        if (obj.classname === 'cells') {
+            sliderInput.addEventListener('input', function () {
+                showValue.textContent = 'value: ' + this.value;
+                var sliderSpeed = dom.getElement('settings__container__speed__input', 0);
+                dom.clearInterval();
+                dom.removeAllChildren(container);
+                create.cells(this.value);
+                dom.changeColor(this.value, sliderSpeed.value);
+            });
+        } else if (obj.classname === 'speed') {
+            sliderInput.addEventListener('input', function () {
+                showValue.textContent = 'value: ' + this.value;
+                var sliderCells = dom.getElement('settings__container__cells__input', 0);
+                dom.clearInterval();
+                dom.changeColor(sliderCells.value, this.value);
+            });
+        } else {
+            sliderInput.addEventListener('input', function () {
+                showValue.textContent = 'value: ' + this.value;
+
+                var cssValue = void 0;
+                switch (obj.classname) {
+                    case 'transition':
+                        cssValue = 'all ' + this.value / 100 + 's';
+                        break;
+                    case 'gridGap':
+                        cssValue = this.value + 'px ' + this.value + 'px';
+                        break;
+                    case 'borderRadius':
+                        cssValue = this.value / 2 + '%';
+                        break;
+                }
+                document.styleSheets[0].rules[obj.cssStyleRule].style[obj.classname] = cssValue;
+            });
+        }
+    };
+
+    var allSliders = function allSliders(obj) {
+        for (var property in obj) {
+            slider(obj[property]);
+        }
+    };
+
+    return {
+        container: container,
+        cells: cells,
+        settingsBar: settingsBar,
+        allSliders: allSliders
+    };
 }();
 
-var createBox = function createBox(numberOfBoxes) {
-    for (var i = 0; i < numberOfBoxes; i++) {
-        var box = dom.createElement('div');
-        dom.setClass(box, 'box');
-        dom.append(container, box);
-    }
-};
-
-var settings = function () {
-    // Create Setting Div //
-    var settings = dom.createElement('div');
-    dom.setClass(settings, 'settings');
-    dom.append(document.body, settings);
-
-    // Create Setting Container Div //
-    var settingsContainer = dom.createElement('div');
-    dom.setClass(settingsContainer, 'settings__container');
-    dom.append(settings, settingsContainer);
-
-    return settingsContainer;
-}();
-
-var transition = {
-    classname: 'transition',
-    title: 'transition',
-    attrs: {
-        class: 'settings__container__transition__input',
-        type: 'range',
-        min: 1,
-        max: 100,
-        value: 1
+var settings = {
+    transition: {
+        classname: 'transition',
+        title: 'transition',
+        attrs: {
+            class: 'settings__container__transition__input',
+            type: 'range',
+            min: 1,
+            max: 100,
+            value: 1
+        },
+        cssStyleRule: 3
     },
-    cssStyleRule: 3
-};
-
-var cells = {
-    classname: 'cells',
-    title: 'number of cells',
-    attrs: {
-        class: 'settings__container__cells__input',
-        type: 'range',
-        min: 1,
-        max: 150,
-        value: 50
-    }
-};
-
-var gap = {
-    classname: 'gridGap',
-    title: 'distance',
-    attrs: {
-        class: 'settings__container__gridGap__input',
-        type: 'range',
-        min: 1,
-        max: 100,
-        value: 1
+    cells: {
+        classname: 'cells',
+        title: 'number of cells',
+        attrs: {
+            class: 'settings__container__cells__input',
+            type: 'range',
+            min: 1,
+            max: 150,
+            value: 50
+        }
     },
-    cssStyleRule: 2
-};
-
-var radius = {
-    classname: 'borderRadius',
-    title: 'Radius',
-    attrs: {
-        class: 'settings__container__borderRadius__input',
-        type: 'range',
-        min: 1,
-        max: 100,
-        value: 10
+    gap: {
+        classname: 'gridGap',
+        title: 'distance',
+        attrs: {
+            class: 'settings__container__gridGap__input',
+            type: 'range',
+            min: 1,
+            max: 100,
+            value: 1
+        },
+        cssStyleRule: 2
     },
-    cssStyleRule: 3
-};
-
-var speed = {
-    classname: 'speed',
-    title: 'Speed',
-    attrs: {
-        class: 'settings__container__speed__input',
-        type: 'range',
-        min: 1,
-        max: 100,
-        value: 50
-    }
-};
-
-// SLIDER //
-var slider = function slider(obj) {
-
-    // create slider-container-div //
-    var slider = dom.createElement('div');
-    dom.setClass(slider, 'settings__container__' + obj.classname);
-    dom.append(settings, slider);
-
-    // create slider-title //
-    var sliderTitle = dom.createElement('h2');
-    sliderTitle.textContent = obj.title;
-    dom.setClass(sliderTitle, 'settings__container__' + obj.classname + '__title');
-    dom.append(slider, sliderTitle);
-
-    // create slider-input //
-    var sliderInput = dom.createElement('input');
-    dom.setAttributes(sliderInput, obj.attrs);
-    dom.append(slider, sliderInput);
-
-    // show current value //
-    var showValue = dom.createElement('p');
-    showValue.textContent = 'value: ' + sliderInput.value;
-    dom.setClass(showValue, 'settings__container__' + obj.classname + '__value');
-    dom.append(slider, showValue);
-
-    if (obj.classname === 'cells') {
-        sliderInput.addEventListener('input', function () {
-            showValue.textContent = 'value: ' + this.value;
-            var sliderSpeed = dom.getElement('settings__container__speed__input', 0);
-            dom.clearInterval();
-            dom.removeAllChildren(container);
-            createBox(this.value);
-            dom.changeColor(this.value, sliderSpeed.value);
-        });
-    } else if (obj.classname === 'speed') {
-        sliderInput.addEventListener('input', function () {
-            showValue.textContent = 'value: ' + this.value;
-            var sliderCells = dom.getElement('settings__container__cells__input', 0);
-            dom.clearInterval();
-            dom.changeColor(sliderCells.value, this.value);
-        });
-    } else {
-        sliderInput.addEventListener('input', function () {
-            showValue.textContent = 'value: ' + this.value;
-
-            var cssValue = void 0;
-            switch (obj.classname) {
-                case 'transition':
-                    cssValue = 'all ' + this.value / 100 + 's';
-                    break;
-                case 'gridGap':
-                    cssValue = this.value + 'px ' + this.value + 'px';
-                    break;
-                case 'borderRadius':
-                    cssValue = this.value / 2 + '%';
-                    break;
-            }
-            document.styleSheets[0].rules[obj.cssStyleRule].style[obj.classname] = cssValue;
-        });
+    radius: {
+        classname: 'borderRadius',
+        title: 'Radius',
+        attrs: {
+            class: 'settings__container__borderRadius__input',
+            type: 'range',
+            min: 1,
+            max: 100,
+            value: 10
+        },
+        cssStyleRule: 3
+    },
+    speed: {
+        classname: 'speed',
+        title: 'Speed',
+        attrs: {
+            class: 'settings__container__speed__input',
+            type: 'range',
+            min: 1,
+            max: 100,
+            value: 50
+        }
     }
 };
 
 var initialCells = 50,
     initialSpeed = 50;
-createBox(initialCells);
-dom.changeColor(container.children.length, initialSpeed);
 
-slider(transition);
-slider(cells);
-slider(gap);
-slider(radius);
-slider(speed);
+create.container();
+create.cells(initialCells);
+create.settingsBar();
+
+dom.changeColor(initialCells, initialSpeed);
+
+create.allSliders(settings);
--- src/script/script.js
+++ src/script/script.js
@@ -62,169 +62,181 @@ let dom = function () {
     }
 }();
 
-let container = function () {
-    let container = dom.createElement('div');
-    dom.setClass(container, 'container');
-    dom.append(document.body, container);
-    return container;
+let create = function () {
+
+    let container = function () {
+        let container = dom.createElement('div');
+        dom.setClass(container, 'container');
+        dom.append(document.body, container);
+    };
+
+    let cells = (numberOfBoxes) => {
+        let container = dom.getElement('container', 0);
+        for (let i = 0; i < numberOfBoxes; i++) {
+            let box = dom.createElement('div');
+            dom.setClass(box, 'box');
+            dom.append(container, box);
+        }
+    };
+
+    let settingsBar = function () {
+        // Create Setting Div //
+        let settings = dom.createElement('div');
+        dom.setClass(settings, 'settings');
+        dom.append(document.body, settings);
+
+        // Create Setting Container Div //
+        let settingsContainer = dom.createElement('div');
+        dom.setClass(settingsContainer, 'settings__container');
+        dom.append(settings, settingsContainer);
+    };
+
+    let slider = function (obj) {
+
+        // create slider-container-div //
+        let slider = dom.createElement('div'),
+            settings = dom.getElement('settings__container', 0),
+            container = dom.getElement('container', 0);
+
+        dom.setClass(slider, `settings__container__${obj.classname}`);
+        dom.append(settings, slider);
+
+        // create slider-title //
+        let sliderTitle = dom.createElement('h2');
+        sliderTitle.textContent = obj.title;
+        dom.setClass(sliderTitle, `settings__container__${obj.classname}__title`);
+        dom.append(slider, sliderTitle);
+
+        // create slider-input //
+        let sliderInput = dom.createElement('input');
+        dom.setAttributes(sliderInput, obj.attrs);
+        dom.append(slider, sliderInput);
+
+        // show current value //
+        let showValue = dom.createElement('p');
+        showValue.textContent = `value: ${sliderInput.value}`;
+        dom.setClass(showValue, `settings__container__${obj.classname}__value`);
+        dom.append(slider, showValue);
+
+        if (obj.classname === 'cells') {
+            sliderInput.addEventListener('input', function () {
+                showValue.textContent = `value: ${this.value}`;
+                let sliderSpeed = dom.getElement('settings__container__speed__input', 0);
+                dom.clearInterval();
+                dom.removeAllChildren(container);
+                create.cells(this.value);
+                dom.changeColor(this.value, sliderSpeed.value);
+            });
+        } else if (obj.classname === 'speed') {
+            sliderInput.addEventListener('input', function () {
+                showValue.textContent = `value: ${this.value}`;
+                let sliderCells = dom.getElement('settings__container__cells__input', 0);
+                dom.clearInterval();
+                dom.changeColor(sliderCells.value, this.value);
+            });
+        } else {
+            sliderInput.addEventListener('input', function () {
+                showValue.textContent = `value: ${this.value}`;
+
+                let cssValue;
+                switch (obj.classname) {
+                    case 'transition':
+                        cssValue = `all ${this.value / 100}s`;
+                        break;
+                    case 'gridGap':
+                        cssValue = `${this.value}px ${this.value}px`;
+                        break;
+                    case 'borderRadius':
+                        cssValue = `${this.value / 2}%`;
+                        break;
+                }
+                document.styleSheets[0].rules[obj.cssStyleRule].style[obj.classname] = cssValue;
+            });
+        }
+    };
+
+    let allSliders = (obj) => {
+        for(let property in obj) {
+            slider(obj[property]);
+        }
+    };
+
+    return {
+        container: container,
+        cells: cells,
+        settingsBar: settingsBar,
+        allSliders: allSliders
+    }
 }();
 
-let createBox = (numberOfBoxes) => {
-    for (let i = 0; i < numberOfBoxes; i++) {
-        let box = dom.createElement('div');
-        dom.setClass(box, 'box');
-        dom.append(container, box);
-    }
-};
-
-let settings = function () {
-    // Create Setting Div //
-    let settings = dom.createElement('div');
-    dom.setClass(settings, 'settings');
-    dom.append(document.body, settings);
-
-    // Create Setting Container Div //
-    let settingsContainer = dom.createElement('div');
-    dom.setClass(settingsContainer, 'settings__container');
-    dom.append(settings, settingsContainer);
-
-    return settingsContainer;
-}();
-
-
-let transition = {
-    classname: 'transition',
-    title: 'transition',
-    attrs: {
-        class: 'settings__container__transition__input',
-        type: 'range',
-        min: 1,
-        max: 100,
-        value: 1
+let settings = {
+    transition: {
+        classname: 'transition',
+        title: 'transition',
+        attrs: {
+            class: 'settings__container__transition__input',
+            type: 'range',
+            min: 1,
+            max: 100,
+            value: 1
+        },
+        cssStyleRule: 3
     },
-    cssStyleRule: 3
-};
-
-let cells = {
-    classname: 'cells',
-    title: 'number of cells',
-    attrs: {
-        class: 'settings__container__cells__input',
-        type: 'range',
-        min: 1,
-        max: 150,
-        value: 50
-    }
-};
-
-let gap = {
-    classname: 'gridGap',
-    title: 'distance',
-    attrs: {
-        class: 'settings__container__gridGap__input',
-        type: 'range',
-        min: 1,
-        max: 100,
-        value: 1
+    cells: {
+        classname: 'cells',
+        title: 'number of cells',
+        attrs: {
+            class: 'settings__container__cells__input',
+            type: 'range',
+            min: 1,
+            max: 150,
+            value: 50
+        }
     },
-    cssStyleRule: 2
-};
-
-let radius = {
-    classname: 'borderRadius',
-    title: 'Radius',
-    attrs: {
-        class: 'settings__container__borderRadius__input',
-        type: 'range',
-        min: 1,
-        max: 100,
-        value: 10
+    gap: {
+        classname: 'gridGap',
+        title: 'distance',
+        attrs: {
+            class: 'settings__container__gridGap__input',
+            type: 'range',
+            min: 1,
+            max: 100,
+            value: 1
+        },
+        cssStyleRule: 2
     },
-    cssStyleRule: 3
-};
-
-let speed = {
-    classname: 'speed',
-    title: 'Speed',
-    attrs: {
-        class: 'settings__container__speed__input',
-        type: 'range',
-        min: 1,
-        max: 100,
-        value: 50
+    radius: {
+        classname: 'borderRadius',
+        title: 'Radius',
+        attrs: {
+            class: 'settings__container__borderRadius__input',
+            type: 'range',
+            min: 1,
+            max: 100,
+            value: 10
+        },
+        cssStyleRule: 3
+    },
+    speed: {
+        classname: 'speed',
+        title: 'Speed',
+        attrs: {
+            class: 'settings__container__speed__input',
+            type: 'range',
+            min: 1,
+            max: 100,
+            value: 50
+        }
     }
-};
-
-
-// SLIDER //
-let slider = function (obj) {
-
-    // create slider-container-div //
-    let slider = dom.createElement('div');
-    dom.setClass(slider, `settings__container__${obj.classname}`);
-    dom.append(settings, slider);
-
-    // create slider-title //
-    let sliderTitle = dom.createElement('h2');
-    sliderTitle.textContent = obj.title;
-    dom.setClass(sliderTitle, `settings__container__${obj.classname}__title`);
-    dom.append(slider, sliderTitle);
-
-    // create slider-input //
-    let sliderInput = dom.createElement('input');
-    dom.setAttributes(sliderInput, obj.attrs);
-    dom.append(slider, sliderInput);
-
-    // show current value //
-    let showValue = dom.createElement('p');
-    showValue.textContent = `value: ${sliderInput.value}`;
-    dom.setClass(showValue, `settings__container__${obj.classname}__value`);
-    dom.append(slider, showValue);
-
-    if (obj.classname === 'cells') {
-        sliderInput.addEventListener('input', function () {
-            showValue.textContent = `value: ${this.value}`;
-            let sliderSpeed = dom.getElement('settings__container__speed__input', 0);
-            dom.clearInterval();
-            dom.removeAllChildren(container);
-            createBox(this.value);
-            dom.changeColor(this.value, sliderSpeed.value);
-        });
-    } else if (obj.classname === 'speed') {
-        sliderInput.addEventListener('input', function () {
-            showValue.textContent = `value: ${this.value}`;
-            let sliderCells = dom.getElement('settings__container__cells__input', 0);
-            dom.clearInterval();
-            dom.changeColor(sliderCells.value, this.value);
-        });
-    } else {
-        sliderInput.addEventListener('input', function () {
-            showValue.textContent = `value: ${this.value}`;
-
-            let cssValue;
-            switch (obj.classname) {
-                case 'transition':
-                    cssValue = `all ${this.value / 100}s`;
-                    break;
-                case 'gridGap':
-                    cssValue = `${this.value}px ${this.value}px`;
-                    break;
-                case 'borderRadius':
-                    cssValue = `${this.value/2}%`;
-                    break;
-            }
-            document.styleSheets[0].rules[obj.cssStyleRule].style[obj.classname] = cssValue;
-        });
-    }
-};
+}
 
 let initialCells = 50,
     initialSpeed = 50;
-createBox(initialCells);
-dom.changeColor(container.children.length, initialSpeed);
 
-slider(transition);
-slider(cells);
-slider(gap);
-slider(radius);
-slider(speed);
+create.container();
+create.cells(initialCells);
+create.settingsBar();
+
+dom.changeColor(initialCells, initialSpeed);
+
+create.allSliders(settings);
